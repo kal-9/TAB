56bb5ac8a1da72f71f62819431d9b05fdcf927de
kal-9 <lance@villagesoftware.co.uk>
2019-01-16 00:05:30 +0000

    onMatch - extra logging

--- serviceworker.js
+++ serviceworker.js
@@ -13,7 +13,15 @@ function onInstall(event)
 function onFetch(event)
 {
     function onMatch(response)
-	{ return response || fetch(event.request) }
+	{ 
+		console.log("cache match !");
+		console.log("event.requeest");
+		console.log("event.request");
+		console.log("response : ");
+		console.log(response);
+
+		return response || fetch(event.request) 
+	}
 
     event.respondWith(caches.match(event.request).then(onMatch));
 }
